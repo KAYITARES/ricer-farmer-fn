2664c140f25735077e60eb6f769b5e8b24975691
iradufrance <Francoiseiradukunda@gmail.com>
2023-12-14 16:41:15 +0200

    add contact page

--- farmer/src/pages/cont.jsx
+++ farmer/src/pages/cont.jsx
@@ -1,20 +1,17 @@
 import React from "react";
-
+import emil from "../assets/image/helloo.png"
 
 export default function Contact(){
     return(
         <>
         <div className="contact-cont">
-        
- <h2>Contact Us</h2>
+     <div className="head">  
+ <h2 className="hello">have some questions</h2>
+ </div> 
 <div className="fom-conto">
  <div className="par">
-<p>Hello every one, we are <span>Rwangingo Rice Farmers</span> 
- <br /><br />We  are so happy because our cooperative <br /><br />its works in 
-coperation and we work hard,<br /><br />
-So if you want to sends us your message and you can <br /><br />
-advice us please fill this form </p>
 
+ <img src={emil} alt="" />
 </div>
  
  <div className="containes">
@@ -25,26 +22,26 @@ advice us please fill this form </p>
            <div className="lbe-container">
 
             <div className="l1">
-  <label htmlFor="">Names:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  {/* <label htmlFor="">Names:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <input type="text" className="Nm" placeholder="enter your full names" />
        </div>
        
        <div className="l2">
-<label htmlFor="">Email:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+{/* <label htmlFor="">Email:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <input type="email" className="email" placeholder="enter your email" /><br></br>
        </div>
        <div className="l3">
-        <label htmlFor="">ContactNumber:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        {/* <label htmlFor="">ContactNumber:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
         <input type="number" className="conmbr" placeholder="enter your contact number"/><br></br>
        </div>
 
 <div className="l4">
-<label htmlFor="">Message:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+{/* <label htmlFor="">Message:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
 &nbsp;
 
-<textarea id="longText" name="longText" rows="2" cols="30"></textarea><br /><br />
+<input type="text" placeholder="write your message" className="textarea" />
 <button type="submit" className="btn">Send</button>
 </div>
 </div>
