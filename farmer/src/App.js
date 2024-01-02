6c41c1f310ee70e8134561439310779bd9daf0f0
iradufrance <Francoiseiradukunda@gmail.com>
2024-01-02 12:46:19 +0200

    afre

--- farmer/src/App.js
+++ farmer/src/App.js
@@ -3,9 +3,11 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Welcome from './pages/welcome';
 import Contact from './pages/cont';
 import SignUp from './pages/signup';
-import Footer from './component/footer';
-import Logo from './component/logo';
+// import Footer from './component/footer';
+// import Logo from './component/logo';
 import "../src/sass/main.scss"
+import Aboutus from './pages/aboutus';
+import Moreabout from './pages/aboutmore';
 function App() {
   return (
  
@@ -14,8 +16,8 @@ function App() {
     <Route path='/' element={<Welcome/>}></Route>
     <Route path='/contact' element={<Contact/>}></Route>
     <Route path='/signup' element={<SignUp/>}></Route>
-    <Route path='/footer' element={<Footer/>}></Route>
-    <Route path='/logo' element={<Logo/>}></Route>
+    <Route path='/aboutus' element={<Aboutus/>}></Route>
+    <Route path='/moreabout' element={<Moreabout/>}></Route>
   </Routes>
 </Router>
 
