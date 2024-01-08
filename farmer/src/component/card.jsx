8e7f7b2542ae40cf6bdbac510a70012e9972e617
mutoni <uwingeneyenazifa29@gmail.com>
2024-01-08 12:23:58 +0200

    add

---
+++ farmer/src/component/card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Card(props){
+    return(
+        <>
+        <div className="card">
+            <p>adsfghjkl;jhjgfdsaSDFGHJ</p>
+        </div>
+        </>
+    )
+}
+
+export default Card
