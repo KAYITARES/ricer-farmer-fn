c3e7fcbb59261a8cb76cc6c9fa54b12263e20c38
mutoni <uwingeneyenazifa29@gmail.com>
2024-01-16 12:04:06 +0200

    add

--- farmer/src/pages/welcome.jsx
+++ farmer/src/pages/welcome.jsx
@@ -1,41 +1,14 @@
 import React from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import '../sass/main.scss';
 import Impact from "../component/impact";
-import Navbar from "../component/header";
-// import Footer from "../component/footer";
-=======
-import '../sass/main.scss'
-=======
-import '../sass/main.scss';
->>>>>>> 6a24abf (add)
-import Navbar from "../component/header";
-<<<<<<< HEAD
-// import Footer from "../component/footer";
-// import Footer from "../component/footer";
->>>>>>> 3d4635f (bfgd)
-
-=======
-import '../sass/main.scss'
 import Navbar from "./header";
->>>>>>> 209cdc6 (as)
-=======
 import Home from "../component/home";
->>>>>>> d45b87d (add)
-
 
 function Welcome(){
 return (
     <>
-  
    <Impact/>
-
-    
     <Navbar/>
-  
-  
    <Home/>
     </>
     )
