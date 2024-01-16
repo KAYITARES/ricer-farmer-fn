13fde9f1fc781f53f10d7ea31050afeaba17a5e2
mutoni <uwingeneyenazifa29@gmail.com>
2024-01-16 11:54:01 +0200

    add

--- farmer/src/pages/welcome.jsx
+++ farmer/src/pages/welcome.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+<<<<<<< HEAD
 import '../sass/main.scss';
 import Impact from "../component/impact";
 import Navbar from "../component/header";
 // import Footer from "../component/footer";
 
+=======
+import '../sass/main.scss'
+import Navbar from "./header";
+>>>>>>> 209cdc6 (as)
 
 
 function Welcome(){
