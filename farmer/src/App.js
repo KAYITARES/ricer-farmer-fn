360cbba1a5851ef97a3b3f18b298d9539e6a5469
mutoni <uwingeneyenazifa29@gmail.com>
2024-01-17 13:05:39 +0200

    add

--- farmer/src/App.js
+++ farmer/src/App.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./component/home"
-=======
+import React from 'react';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
->>>>>>> 949988a (add)
 import Welcome from './pages/welcome';
 import Contact from './pages/cont';
 import SignUp from './pages/signup';
@@ -18,8 +12,6 @@ import Product from './pages/product';
 import Single from './pages/Singleproduct';
 import Login from './pages/login';
 import Products from "./pages/product";
-
-
 import Impact from './pages/impact';
 import Kuvuruga from './pages/kuvuruga';
 import Guteraa from './pages/guteraa';
@@ -27,34 +19,22 @@ import Kubagara from './pages/kubagara';
 import Weze from './pages/weze';
 import Kwanika from './pages/kwanika';
 import Kubika from './pages/kubika';
-<<<<<<< HEAD
 
 
-
-=======
-
-import React from 'react';
-
-import './sass/main.scss';
-
-
->>>>>>> 949988a (add)
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Welcome />}></Route>
-        <Route path="/HOME" element={<Home/>}></Route>
-    <Route path='/' element={<Welcome/>}></Route>
+
+    <Route path="/" element={<Welcome />}></Route>
     <Route path='/contact' element={<Contact/>}></Route>
     <Route path='/signup' element={<SignUp/>}></Route>
     <Route path='/aboutus' element={<Aboutus/>}></Route>
     <Route path='/moreabout' element={<Moreabout/>}></Route>
-    <Route path='/home' element={<Home/>}></Route>
     <Route path='/homemore' element={<Homemore/>}></Route>
     <Route path='/footer' element={<Footer/>}></Route>
     <Route path='product' element={<Products/>}></Route>
-    <Route path='/welcome' element={<Welcome/>}></Route>
+
     <Route path='/product' element={<Product/>}></Route>
     <Route path='/Singleproduct' element={<Single/>}></Route>
     <Route path='/login' element={<Login/>}></Route>
@@ -71,10 +51,6 @@ function App() {
 
   
   );
-<<<<<<< HEAD
-  
-=======
->>>>>>> 949988a (add)
 
 }
 export default App;
